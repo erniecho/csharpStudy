894e99647ae1cfb967c342070e8e8a3224319a3b
erniecho <erniecho@gmail.com>
2019-04-09 15:27:15 -0400

    Chapter 1 pg 22

--- Helloworld/Program.cs
+++ Helloworld/Program.cs
@@ -3,6 +3,15 @@ class HelloWorld
     static void Main()
     {
         System.Console.WriteLine("Hello. My name is Inigo Montoya. ");
+        string valerie;
+        string miracleMax = "Have fun storming the castle!";
+
+        valerie = "Think it will work?";
+        System.Console.WriteLine(miracleMax);
+        System.Console.WriteLine(valerie);
+
+        miracleMax = "It would take a miracle.";
+        System.Console.WriteLine(miracleMax);
+
     }
 }
-
