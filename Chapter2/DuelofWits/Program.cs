ac421382b8d83b32ba5a883811527d88c17e8333
erniecho <erniecho@gmail.com>
2019-04-12 12:34:13 -0400

    12 apr 2019 working on datatypes

---
+++ Chapter2/DuelofWits/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DuelofWits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.Write(
+            "\"Truly, you have a dizzying intelect.\"" );
+            System.Console.Write("\n\"Wait 'til I get going!\"\n");
+        }
+    }
+}
+
---
+++ Chapter2/Listing29/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Listing29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            const double number = 1.618033988749895;
+            double result;
+            string text;
+
+            text = $"{number}";
+            result = double.Parse(text);
+            System.Console.WriteLine($"{result == number}: result == number");
+
+            text = string.Format("{0:R}", number);
+            result = double.Parse(text);
+            System.Console.WriteLine($"{result == number}: result == number");
+        }
+    }
+}
