6cc3b5d72bf4bce56efafbe8a6d5df1996a79f37
erniecho <erniecho@gmail.com>
2019-04-13 15:55:19 -0400

    using static directive

---
+++ Chapter2/listing215/Program.cs
@@ -0,0 +1,23 @@
+﻿using static System.Console; //This is a way to using Static Directive so you don't have to type the whole line.
+
+namespace listing215
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstname;
+            string lastName;
+
+            WriteLine("Hey You");
+
+            Write("Enter your first name: ");
+            firstname = ReadLine();
+            Write("Enter your last name: ");
+            lastName = ReadLine();
+
+            WriteLine(
+            $"Your full name is {firstname} {lastName}");
+        }
+    }
+}
