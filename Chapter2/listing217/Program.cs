3b38810ed6769d5142dcfa1ef1055f6b270922cf
erniecho <erniecho@gmail.com>
2019-04-13 16:26:01 -0400

    added code from pg 66 example

---
+++ Chapter2/listing217/Program.cs
@@ -0,0 +1,20 @@
+﻿using static System.Console;
+
+namespace listing217
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text;
+
+            Write("Enter text: ");
+            text = ReadLine();
+
+            //UNEXPECTED: DOES NOT CONVERT TEXT TO UPPERCASE.
+            text.ToUpper();
+
+            WriteLine(text);
+        }
+    }
+}
