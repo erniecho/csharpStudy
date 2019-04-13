c3aaa438a3d2009a136f41aded5e2163ff6e18e6
erniecho <erniecho@gmail.com>
2019-04-13 16:18:36 -0400

    this is listing 216 code pg65

---
+++ Chapter2/listing216/Program.cs
@@ -0,0 +1,20 @@
+﻿using static System.Console;
+
+namespace listing216
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string palindrome;
+
+            Write("Enter a palindrome: ");
+            palindrome = ReadLine();
+
+            WriteLine(
+            $"The palindrome \"{palindrome}\" is"
+            + $" { palindrome.Length } characters."
+                );
+        }
+    }
+}
