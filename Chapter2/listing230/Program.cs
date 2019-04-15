7fc2828e1e3c5695bf4c815a96b7a0f569170264
erniecho <erniecho@gmail.com>
2019-04-15 18:26:52 -0400

    this is chapter 2 example listing 230

---
+++ Chapter2/listing230/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace listing230
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //double number;
+            string input;
+
+            System.Console.Write("Enter a number: ");
+            input = System.Console.ReadLine();
+            if (double.TryParse(input, out double number))
+            {
+                System.Console.WriteLine(
+                $"input was parsed successfully to {number}.");}
+            else
+            {
+                //Note: number scope is here too (although not assigned)
+                System.Console.WriteLine(
+                "The text entred was not a valid number.");
+            }
+
+        }
+    }
+}
