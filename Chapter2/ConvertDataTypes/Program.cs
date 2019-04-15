56429e5b2811f67bbd51682be968559f0ab8889c
erniecho <erniecho@gmail.com>
2019-04-15 18:06:07 -0400

    code from page73

---
+++ Chapter2/ConvertDataTypes/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConvertDataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long longNumber = 50918309109;
+            int intNumber = (int)longNumber; //Explicit cast operator.
+
+            unchecked
+            {
+                // int.MaxValue equals 2147483647
+                int n = int.MaxValue;
+                n = n + 1;
+                System.Console.WriteLine(n);
+            }
+            intNumber = 31416;
+            longNumber = (long)intNumber;
+
+            string text = "9.11E-31";
+            float kgElectronMass = float.Parse(text); //String to Float
+
+            string middleCText = "261.626";
+            double middleC = System.Convert.ToDouble(middleCText);
+
+        }
+    }
+}
