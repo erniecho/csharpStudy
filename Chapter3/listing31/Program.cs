2c4605ce17810d74867ff329a0edd8b50fbb258a
erniecho <erniecho@gmail.com>
2019-04-17 16:17:02 -0400

    Listing code 32 pg81

---
+++ Chapter3/listing31/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace listing31
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         /*   int? count = null;
+
+            do
+            {
+                //procudure code here.
+            } while (count == null);  */
+
+            System.Console.Write("Enter text: ");
+            var text = System.Console.ReadLine();
+
+            // Return a new string in uppercase
+            var uppercase = text.ToUpper();
+
+            System.Console.WriteLine(uppercase);
+        }
+    }
+}
