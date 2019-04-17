c633d65aff20843e3e0f7886243e66d1d0c06a0d
erniecho <erniecho@gmail.com>
2019-04-17 17:08:41 -0400

    listing 320 pg98

---
+++ Chapter3/listing320/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace listing320
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] cells =
+            {
+                {1,0,2},
+                {0,2,0},
+                {1,2,1}
+            };
+
+            cells[1, 0] = 1;
+        }
+    }
+}
