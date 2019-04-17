d8f507f549e5041b0ecfe01394f1ab22987771da
erniecho <erniecho@gmail.com>
2019-04-17 16:25:12 -0400

    listing 33 code pg82

--- Chapter3/listing31/Program.cs
+++ Chapter3/listing31/Program.cs
@@ -20,6 +20,17 @@ namespace listing31
             var uppercase = text.ToUpper();
 
             System.Console.WriteLine(uppercase);
+
+            var patent1 =
+                new { Title = "Bifocals", YearOfPublication = "1784" };
+            var patent2 =
+                new {Title = "Phonograph", YearOfPublication = "1877" };
+
+            System.Console.WriteLine( 
+            $"{ patent1.Title} ({ patent1.YearOfPublication })");
+
+            System.Console.WriteLine(
+            $"{ patent2.Title} ({ patent2.YearOfPublication })");
         }
     }
 }
