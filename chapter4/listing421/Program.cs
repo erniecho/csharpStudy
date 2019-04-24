8da596283137050cbaec7c38f39e732cab6fcdd7
erniecho <erniecho@gmail.com>
2019-04-24 16:39:42 -0400

    coding examples from chapter 4

---
+++ chapter4/listing421/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace listing421
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input; //declare a variable to store the input
+
+            System.Console.Write(
+            "What is the maximum number" +
+            "of turns in tic-tac-toe?" +
+            "(Enter 0 to exit.): "
+            );
+
+            // int.Parse() converts the ReadLine()
+            // return to an int data type
+            input = int.Parse(System.Console.ReadLine());
+
+            if (input <= 0) // line 16
+            // Input is less than or equal to 0
+            System.Console.WriteLine("Exiting...");
+            else
+            if (input < 9) //Line 20
+            //Input is less than 9
+            System.Console.WriteLine(
+            $"Tic-tac-toe has more than {input}" +
+            " maximum turns.");
+        }
+    }
+}
