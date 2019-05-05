d4031cd17a90c87701f84a55350bf4b50015ef81
erniecho <erniecho@gmail.com>
2019-05-05 19:03:26 -0400

    this is example listing437

---
+++ chapter4/listing437/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace listing437
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args?.Length == 0)
+            {
+              System.Console.WriteLine(
+              "ERROR: File missing. "
+              + "Use:\n\tfind.exe file:<filename>");
+            }
+            else
+            {
+              if (args[0]?.ToLower().StartsWith("file:")??false)
+              {
+                string fileName = args[0]?.Remove(0, 5);
+                // ...
+              }
+            }
+        }
+    }
+}
